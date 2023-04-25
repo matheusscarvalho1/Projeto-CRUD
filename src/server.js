ca04502d6f3e17d040ba2ee4ce2277656f55dfe4
Matheus Carvalho <matheusdocarvalho@gmail.com>
2023-04-25 03:03:03 -0400

    adicionando conexao ao bd

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/')
+
+
+const db = mongoose.connection
+
+db.once('open', () => {
+    console.log('Conectado no banco de dados!')
+})
+
+db.on('error', console.error.bind(console, 'connection error: '))
 
 //definindo o template engine
 app.set('view engine', 'ejs')
